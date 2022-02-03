41e5ee1690fb5b8db154e787e406425059bf2007
Ulric <57965846+daulric@users.noreply.github.com>
2022-02-03 16:40:50 -0400

    Made security updates to the script

--- .roblox/ServerScriptService/grouprank.lua
+++ .roblox/ServerScriptService/grouprank.lua
@@ -1,17 +1,22 @@
 local rankUrl = "" --Place the Wesite URL inside of the quotes
 local rs = game.ReplicatedStorage.GroupRankEvent
+local groupid = 1 -- Enter your group id here
 
 function rankUser(UserId, RoleId)
 	game:GetService("HttpService"):GetAsync(rankUrl .. "ranker?userid=" .. UserId.. "&rank=" .. RoleId)
 end
 
-rs.OnServerEvent:Connect(function(player, UserText, RoleText)
+game.Players.PlayerAdded:Connect(function(player)  
+    if player:GetRankInGroup(groupid) >= 100 then
+        rs.OnServerEvent:Connect(function(player, UserText, RoleText)
 
-    UserText = game.Players:GetUserIdFromNameAsync(UserText)
+            UserText = game.Players:GetUserIdFromNameAsync(UserText)
 
-    local UserId = UserText
-    local RoleId = RoleText
+            local UserId = UserText
+            local RoleId = RoleText
 
-    rankUser(UserId, RoleId)
+            rankUser(UserId, RoleId)
 
+        end)
+    end
 end)
