46546ce22e2af70b26cc5f06c5d585705c5506c6
Ulric <57965846+daulric@users.noreply.github.com>
2022-02-10 13:19:20 -0400

    Update gui.lua
    more updates

--- .roblox/StarterGui/gui.lua
+++ .roblox/StarterGui/gui.lua
@@ -1,7 +1,7 @@
 local UserText = script.Parent.Parent.UserText
 local RoleText = script.Parent.Parent.RoleText
 local promoteButton = script.Parent
-local rs = game.ReplicatedStorage.GroupRankEvent
+local rs = game.ReplicatedStorage.GroupRankEvent -- Enter where the event is located
 
 promoteButton.MouseButton1Click:Connect(function()
     rs:FireServer(UserText.Text, RoleText.Text)
